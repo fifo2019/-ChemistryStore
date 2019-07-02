1dc2e15976f65bcb7f428eb7a2a826afb67df631
Pavel <pavel.dudkov@icloud.com>
2019-07-02 17:37:44 +0400

    1. Добавил декораторы для корзины, чтобы неавторизированные пользоваетль не смог пользоваться корзиной. 2. Отладил работу пользователя, при выборе товара, если он не авторизированные перебрасывает на страницу с авторизации, после авторизации, возвращает для дальнейшей процедуры с товаром. 3. Добавил библиотеку jquery в статику

--- mysite/basketapp/views.py
+++ mysite/basketapp/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, HttpResponseRedirect, get_object_or_404
 from basketapp.models import Basket
 from mainapp.models import Product
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
 
 
+@login_required
 def basket(request):
     title = 'Корзина'
     basket_items = Basket.objects.filter(user=request.user).order_by('product__category')
@@ -15,7 +18,11 @@ def basket(request):
     return render(request, 'basketapp/basket.html', content)
 
 
+@login_required
 def basket_add(request, pk):
+    if 'login' in request.META.get('HTTP_REFERER'):
+        return HttpResponseRedirect(reverse('products:product', args=[pk]))
+
     product = get_object_or_404(Product, pk=pk)
 
     basket = Basket.objects.filter(user=request.user, product=product).first()
@@ -29,6 +36,7 @@ def basket_add(request, pk):
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 
+@login_required
 def basket_remove(request, pk):
     basket_record = get_object_or_404(Basket, pk=pk)
 
