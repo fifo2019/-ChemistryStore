3e3ecccd34b6fcd81e79cab74db74faa2fbc0c60
Pavel <pavel.dudkov@icloud.com>
2019-07-12 00:33:10 +0400

    1. Допилил админку, теперь всё отражается в ней

--- mysite/basketapp/admin.py
+++ mysite/basketapp/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from .models import Basket
 
-admin.site.register(Basket)
+
+class BasketAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Basket._meta.fields]
+
+    class Meta:
+        model = Basket
+
+
+admin.site.register(Basket, BasketAdmin)
--- mysite/mainapp/admin.py
+++ mysite/mainapp/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
 from .models import ProductCategory, Product
 
-admin.site.register(ProductCategory)
-admin.site.register(Product)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["name", "category", "image", "short_desc", "description", "price"]
+    list_filter = ["category"]
+    search_fields = ["name", "short_desc", "description", "price"]
+
+    class Meta:
+        model = Product
+
+
+class ProductCategoryAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in ProductCategory._meta.fields]
+
+    class Meta:
+        model = ProductCategory
+
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(ProductCategory, ProductCategoryAdmin)
